4b60e6cb7b2d864b09fd7081d124e4d52b0ad7c8
KonradGumowski <konrad.gumowski@pw.edu.pl>
2023-10-30 18:52:39 +0100

    5

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -11,13 +11,29 @@ class ResourceManager
 public:
     ResourceManager() { a = 10; 
     std::cout<<"hello"<<std::endl;
-    Resource moje_zasoby();
+    Resource moje_zasoby{};
     }
     ~ResourceManager() {}
 
     double get() {
-        return a;
+        return 0;//moje_zasoby.get();
     }
 private:
     double a;
+};
+
+class Wektor{
+public:
+    Wektor(int n):dlugosc(n){
+        wektor=new double[dlugosc];
+        for(int i=0; i<dlugosc; i++) wektor[i]=0;
+    }
+    ~Wektor(){
+        delete[] wektor;
+    }
+    void print(){
+        for(int i=0; i<dlugosc; i++) std::cout<<wektor[i]<<std::endl;
+    }
+    double* wektor;
+    int dlugosc;
 };
