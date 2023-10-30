82516954c8f66aef2b56e0f49e178a5516ad3078
KonradGumowski <konrad.gumowski@pw.edu.pl>
2023-10-30 15:53:58 +0100

    2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,12 +1,13 @@
 #pragma once
-
+#include<iostream>
 #include "Resource.hpp"
 
 class ResourceManager
 {
     // Twoja implementacja tutaj
 public:
-    ResourceManager() { a = 10;    }
+    ResourceManager() { a = 10; 
+    std::cout<<"hello"<<std::endl;   }
     ~ResourceManager() {}
 
     double get() {
